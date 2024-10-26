00f578ea8f117bb5bcc6b6367ecc688364542f56
Vishane Stubbs <stubbsvishane@gmail.com>
2024-10-26 17:34:39 -0500

    Exercise 3

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,17 +5,35 @@ document.addEventListener("DOMContentLoaded",function(){
     const sqaures= document.querySelectorAll("#board div");
     sqaures.forEach(div=> div.className="square");
     var initial= "X";
+
+
+    /* Exercise 2 */
     for (const item of sqaures){
         item.addEventListener("click", function(){
             if (item.textContent==""){
                 item.textContent= initial;
                 item.classList.add(initial);
+                initial= initial== "X"? "O": "X";
+                
                 
             }
-        initial= initial== "X"? "O": "X";
+        
 
         })
     }
+    /*Exercise 3 */
+    for (const item of sqaures){
+        item.addEventListener("mouseover",function(){
+            item.classList.add("hover");
+    
+        })
+
+        item.addEventListener("mouseout", function(){
+            item.classList.remove("hover");
+        })
+    }
+
+
 });
 
 
