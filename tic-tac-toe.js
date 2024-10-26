b2b5dad29a9af9572a0b43698260e3aacd2ecbe1
Vishane Stubbs <stubbsvishane@gmail.com>
2024-10-26 16:49:52 -0500

    Implemented Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,6 +4,20 @@
 document.addEventListener("DOMContentLoaded",function(){
     const sqaures= document.querySelectorAll("#board div");
     sqaures.forEach(div=> div.className="square");
+    var initial= "X";
+    for (const item of sqaures){
+        item.addEventListener("click", function(){
+            if (item.textContent==""){
+                item.textContent= initial;
+                item.classList.add(initial);
+                
+            }
+        initial= initial== "X"? "O": "X";
+
+        })
+    }
 });
 
-iti
+
+
+
