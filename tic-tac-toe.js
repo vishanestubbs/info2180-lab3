e6a0c95b1238c5b20805bdecb69eef535a89d2b4
Vishane Stubbs <stubbsvishane@gmail.com>
2024-10-26 19:26:07 -0500

    Completed Exercises 4,5,6

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,20 +1,30 @@
 
 /* Loads the games sqaures on to the board */
-
 document.addEventListener("DOMContentLoaded",function(){
-    const sqaures= document.querySelectorAll("#board div");
+    var sqaures= Array.from(document.querySelectorAll("#board div"));
     sqaures.forEach(div=> div.className="square");
-    var initial= "X";
+    sqaures.forEach(div=>div.id="")
 
+    var gameState = Array(9).fill(null);
+    var player= "X";
+
+    
 
     /* Exercise 2 */
     for (const item of sqaures){
         item.addEventListener("click", function(){
             if (item.textContent==""){
-                item.textContent= initial;
-                item.classList.add(initial);
-                initial= initial== "X"? "O": "X";
+                item.textContent= player;
+                item.classList.add(player);
+                gameState[sqaures.indexOf(item)]=player;
+                console.log(gameState);
                 
+                if (winner(player,gameState)){
+                   let status= document.getElementById("status");
+                   status.innerHTML= `<i>Congratulations! ${player} is the winner</i>`;
+                   status.classList.add("you-won");   
+                }
+                player= player== "X"? "O": "X";
                 
             }
         
@@ -33,8 +43,32 @@ document.addEventListener("DOMContentLoaded",function(){
         })
     }
 
+    let restart= document.getElementsByTagName("button")[0];
+    restart.addEventListener("click",function(){
+        gameState=Array(9).fill(null);
+        sqaures.forEach(div=> div.className="square");
+        sqaures.forEach(div=>div.textContent="");
+        let status= document.getElementById("status");
+        status.innerHTML="Move your mouse over a square and click to play an X or an O.";
+        status.className="status";
+}) 
 
 });
+
+const winningCombinations =[
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+    [0, 4, 8], [2, 4, 6]  ] // Diagonals
+
+function winner(player,list){
+    return winningCombinations.some(c=> c.every(idx=>list[idx]==player))
+}
+
+
+
+
+
+
 
 
 
